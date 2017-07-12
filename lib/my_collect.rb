f80e38833fd14850f30305a0c1a5a245e9176092
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-11 21:34:10 -0400

    collections

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(collection)
+    		   index = 0
+      new_collectionz = []
+      while index < collection.length
+        new_item = yield(collection[index])
+        new_collectionz << new_item
+    index += 1
+      end
+ new_collectionz
+end
 
